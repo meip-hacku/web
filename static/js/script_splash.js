6758d58fd7989e89d68edc0f85c2609e44edb7c9
kosukehorn <kosukehorn0805@gmail.com>
2024-02-24 11:02:02 +0900

    コメントを整理

--- static/js/script_splash.js
+++ static/js/script_splash.js
@@ -1,15 +1,14 @@
 document.getElementById('splash-start').addEventListener('click', function() {
     this.style.display = 'none';
     
-    // 音声ファイルのパス
+    // 音声・画像ファイルのパス
     var audio = new Audio('static/audio/GanbariMascleTitle.wav');
-
     var img = document.getElementById('logoImage');
+    
     img.style.display = 'block'; // 画像を表示
     img.style.opacity = 0; // 透明度を0に設定
 
     var opacity = 0;
-    var audio = new Audio('/static/audio/GanbariMascleTitle.wav'); // 音声ファイルのパス
     var itr_num = 0;
 
     var intervalID = setInterval(function() {
@@ -20,18 +19,18 @@ document.getElementById('splash-start').addEventListener('click', function() {
         } else {
             clearInterval(intervalID); // フェードイン完了後にインターバルをクリア
         }
-        if (itr_num == 165) {
+        if (itr_num == 165) { // 程よいところで音声を再生
             audio.play().then(() => {
                 console.log("Audio started successfully");
             }).catch(error => {
                 console.error("Playback failed:", error);
             });
         }
-        if (itr_num > 165) {
+        if (itr_num > 165) { // 自動で計測画面へ遷移
             clearInterval(intervalID); // インターバルをクリア
             setTimeout(function() {
                 window.location.href = "/play"; // メインページへのパス
-            }, 1700); //1500ミリ秒後に遷移
+            }, 1700);
         }
     }, 10); // 10ミリ秒ごとに透明度を更新
 });
